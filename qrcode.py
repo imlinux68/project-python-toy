732a579ee38f1ac7ae82248fd214430fa95cc825
im linux <imlinux68@gmail.com>
2023-08-24 17:50:10 +0300

    completed qr code

--- qrcode.py
+++ qrcode.py
@@ -3,4 +3,11 @@
 import pyqrcode
 
 
+def qrcode():
+    q=pyqrcode.create(input())
+    q.png('qrcode.png', scale=6)
+    print('QR Code Generated Successfully!')
 
+
+if __name__=='__main__':
+    qrcode()
